df8ea2f28a1488e6a311684fa9f4900b86e960e1
clarinhamartins <mariaclaraalmeidamartins06@gmail.com>
2026-01-03 16:26:15 -0300

    feat: implementado escalonamento inteligente de porções sem persistência

--- src/core/interfaces/IRecipeService.ts
+++ src/core/interfaces/IRecipeService.ts
@@ -6,4 +6,6 @@ export interface IRecipeService {
   create(input: CreateRecipeInput): Promise<Recipe>
   update(id: string, data: Partial<CreateRecipeInput>): Promise<Recipe>
   delete(id: string): Promise<void>
+
+  scaleRecipe(id: string, servings: number) : Promise<Recipe>;
 }
